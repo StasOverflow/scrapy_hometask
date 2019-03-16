9ccf264913929a517550c72da4653092fbdea3b3
Vladimir <vladimir.alexeevich.morrowind@gmail.com>
2019-03-16 11:52:32 +0200

    working on spider

--- scrapy_parse/spiders/aizel.py
+++ scrapy_parse/spiders/aizel.py
@@ -8,10 +8,18 @@ class AizelClothSpider(RedisSpider):
     # start_urls = ("https://aizel.ru/ua-ru/odezhda/bryuki/", )
 
     # def make_request_from_data(self, data):
+    size_base_url = 'https://aizel.ru/products/sizes/?id='
 
-    def get_sizes(self):
-        # https://aizel.ru/products/sizes/?id=
+    def field_format(self):
         pass
+
+    def get_color(self, response):
+        """
+        Checks if a field parsed successfully
+        :return: Field value, if parsed, Nothing if parsing failed
+        """
+        return response.xpath('//div[@class="details__row"]/'
+                              'span[contains(text(), "Цвеgт")]/../text()').get()
 
     def parse(self, response):
         link = response.xpath('//ul[@class="pagination"]/li[last()]/a/@href').get()
@@ -34,31 +42,32 @@ class AizelClothSpider(RedisSpider):
         #     yield scrapy.Request(response.urljoin(link), self.parse_cloth_item)
         for index, link in enumerate(cloth_link_list[1:3]):
             item_id = link.split('-')[-1].strip('/')
-            yield scrapy.Request(response.urljoin(link), self.parse_cloth_item, meta={'item_id': item_id})
+            yield scrapy.Request(response.urljoin(link), self.parse_cloth_fields, meta={'item_id': item_id})
 
-    def parse_cloth_item(self, response):
-        print('AYDI IS ', response.meta['item_id'])
-        brand = response.xpath('//h1[@itemprop="name"]/a/text()').get()
-        title = response.xpath('//h1[@itemprop="name"]/span/text()').get()
-        image = response.xpath('//img[@itemprop="image"]/@src').get()
-        price = response.xpath('//span[contains(@itemprop, "price")]/text()').get()
+    def parse_cloth_fields(self, response):
+        meta_dict = dict()
+        meta_dict['brand'] = response.xpath('//h1[@itemprop="name"]/a/text()').get()
+        meta_dict['title'] = response.xpath('//h1[@itemprop="name"]/span/text()').get()
+        meta_dict['image'] = response.xpath('//img[@itemprop="image"]/@src').get()
+        meta_dict['price'] = response.xpath('//span[contains(@itemprop, "price")]/text()').get()
+        meta_dict['descr'] = response.xpath('//p[contains(@itemprop, "description")]/text()').get()
+        # meta_dict['color'] = self.get_color(response)
 
-        '''
-            The following code snippet returns nothing, because its content is added to page via js
-            
-            sizes = response.xpath('//li[contains(@class, "size")]/span[@class="product-size-title"]/text()').get()
-        '''
-        # size = response.xpath('//div[@class="details__row"]/text()')[2].get().strip().split(' ')[-1:]
+        meta_dict['color'] = response.xpath('//div[@class="deta2ils__row"]/'
+                                            'span[contains(text(), "Цвеgт")]/../text()').get()
 
-        descr = response.xpath('//p[contains(@itemprop, "description")]/text()').get()
-        color = response.xpath('//div[@class="details__row"]/span[contains(text(), "Цвет")]/../text()').get()
+        size_request_url = self.size_base_url + response.meta['item_id'] + '/'
+        return scrapy.Request(size_request_url, self.parse_cloth_item_with_size, meta=meta_dict)
+
+    def parse_cloth_item_with_size(self, response):
 
         fields_item = AizelClothItem()
-        fields_item['brand'] = brand
-        fields_item['title'] = title
-        fields_item['image'] = response.urljoin(image)
-        fields_item['price'] = price
-        # fields_item['size'] = size
-        fields_item['descr'] = descr
-        fields_item['color'] = color
+        fields_item['brand'] = response.meta['brand']
+        fields_item['title'] = response.meta['title']
+        fields_item['image'] = response.urljoin(response.meta['image'])
+        fields_item['price'] = response.meta['price']
+        fields_item['size'] = response.xpath('//ul[contains(@class, "size__list")]//'
+                                             'span[@class="product-size-title"]/text()').getall()
+        fields_item['descr'] = response.meta['descr']
+        fields_item['color'] = response.meta['color']
         print(fields_item)
